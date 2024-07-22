75c3f09705cfbfb83213d61dd97abc8d54b76c90
Sachin Shetty <26170834+sachinsshetty@users.noreply.github.com>
2024-07-22 16:59:46 +0000

    fix pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
+
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  devOptions: {
+    enabled: true
+  },
+  includeAssets: ['drishti.jpg'],
+  manifest: {
+    name: 'Drishti',
+    short_name: 'Drishti',
+    theme_color: '#242424',
+    icons: [
+      {
+        src: 'pwa/pwa-192-192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa/pwa-512-512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,25 +40,6 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate',
-      manifest: {
-        name: 'My Awesome App',
-        short_name: 'MyApp',
-        description: 'My Awesome App description',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-     })
+    VitePWA(pwaOptions)
   ],
 })
